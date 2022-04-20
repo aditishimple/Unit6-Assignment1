e81160c368e6a2c064a476262208148fc9b5b0fa
Aditi Shimple <aditishimple22@gmail.com>
2022-04-20 11:38:18 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const add = require('./add')
+const sub = require('./subtract')
+const mul = require('./multiply')
+const div = require('./divide')
+
+console.log(add.sum(10, 5));
+console.log(sub.sub(10, 5));
+console.log(mul.mul(10, 5));
+console.log(div.div(10, 5));
